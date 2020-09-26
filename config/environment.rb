b679614de2f3bd08e01aa543674afbebe5ee9d40
SaiPraveen912 <saipraveen.immanni@gmail.com>
2020-09-26 13:06:55 -0400

    Style validation and flash messages using bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Disables div -> field with errors provided by rails
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
